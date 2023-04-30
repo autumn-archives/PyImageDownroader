03193e6f4d1c1c0f10c5b1e3a467ca9d38e862dc
autumn-archives <ko5ok1a2r3a44kiyanv4@outlook.jp>
2023-04-30 15:29:39 +0900

    GUIをクラス構造に

--- main.py
+++ main.py
@@ -49,44 +49,47 @@ class ImageDownloader:
         except:
             return False
 
-def main():
+
+class GUIView:
+    def __init__(self):
     # GUIのレイアウト
-    layout = [
-    [sg.Frame("ファイルパスの入力", [
-        [sg.Input(size=(45, 1), key="-input_folder_path-"),sg.FolderBrowse("参照")]
-    ])],
-    [sg.Frame("画像のダウンロード", [
-        [sg.TabGroup([
-            [sg.Tab('URLからダウンロード', [
-                [sg.Input(size=(45, 1), key="-input_url-"),sg.Button("ダウンロード",key='-download-')],
-                [sg.Multiline(size=(70, 5), key="output_url", autoscroll=True)]
-            ]),
-            sg.Tab('ページ全体からダウンロード', [
-                [sg.Input(size=(45, 1), key="-input_url-_all-"),sg.Button("ダウンロード")],
-                [sg.Multiline(size=(70, 5), key="output_url_all", autoscroll=True)]
+        layout = [
+            [sg.Frame("ファイルパスの入力", [
+                [sg.Input(size=(45, 1), key="-input_folder_path-"),sg.FolderBrowse("参照")]
+            ])],
+            [sg.Frame("画像のダウンロード", [
+                [sg.TabGroup([
+                    [sg.Tab('URLからダウンロード', [
+                        [sg.Input(size=(45, 1), key="-input_url-"),sg.Button("ダウンロード",key='-download-')],
+                        [sg.Multiline(size=(70, 5), key="output_url", autoscroll=True)]
+                    ]),
+                    sg.Tab('ページ全体からダウンロード', [
+                        [sg.Input(size=(45, 1), key="-input_url-_all-"),sg.Button("ダウンロード")],
+                        [sg.Multiline(size=(70, 5), key="output_url_all", autoscroll=True)]
+                    ])]
+                ])]
             ])]
-        ])]
-    ])]
-]
+        ]
+            # ウィンドウを作成
+        self.window = sg.Window("GUI", layout)
 
+    def main(self):
         
-    
-    # ウィンドウを作成
-    window = sg.Window("GUI", layout)
 
-    # イベントループ
-    while True:
-        event, values = window.read()
-        if event == sg.WINDOW_CLOSED:
-            break
-        elif event == "-download-":
-            pass
-        elif values["-input_url-_all-"]:
-            pass
+        # イベントループ
+        while True:
+            event, values = self.window.read()
+            if event == sg.WINDOW_CLOSED:
+                break
+            elif event == "-download-":
+                pass
+            elif values["-input_url-_all-"]:
+                pass
 
 
-    # ウィンドウを閉じる
-    window.close()
+        # ウィンドウを閉じる
+        self.window.close()
 
 if __name__ == "__main__":
-    main()
+    image_downloader = GUIView()
+    image_downloader.main()
